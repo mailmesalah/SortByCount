d9f40a8537b2f01a0382b67076310fd9c7bf2b63
Sely <mailmesalah@gmail.com>
2022-09-27 14:22:21 +0100

    First Commit

---
+++ SortByCount/src/sortbycount/ValueAndCount.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortbycount;
+
+/**
+ *
+ * @author DeXtop
+ */
+public class ValueAndCount {
+    public int value=0;
+    public int count=0;
+}
